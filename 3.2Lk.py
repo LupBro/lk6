5ddbee95d9a6e0964c2211b184294316ae552d92
LupBro <abizhanov01@mail.ru>
2022-02-21 00:40:24 +0300

    3.2Lk.py

---
+++ 3.2Lk.py
@@ -0,0 +1,13 @@
+a = int(input("Введи число целое"))
+b = float(input("Введи число дробное"))
+c = int(input("Введи число целое"))
+S = a**2 + b**2
+A = 3 * b - 4
+D = S / A
+C = (4 * c**5) / 6
+V = D / C
+M = D // C
+N =  D % C
+print(V)
+print(M)
+print(N)
